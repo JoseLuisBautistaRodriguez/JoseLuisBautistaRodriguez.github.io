4524d8eff3ef95924f91e93fcdecee6bbbf66ef0
José Luis Bautista Rodríguez <joseluisbaur@gmail.com>
2024-10-16 23:15:48 -0600

    Changes in studies

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,17 +1,22 @@
-const carrusel = document.querySelector('.carrusel');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
+// Selección y manejo de múltiples carruseles
+const carruselContainers = document.querySelectorAll('.carrusel-container');
 
-prevBtn.addEventListener('click', () => {
-    carrusel.scrollBy({
-        left: -carrusel.offsetWidth / 2, // Mueve una imagen
-        behavior: 'smooth'
+carruselContainers.forEach(container => {
+    const carrusel = container.querySelector('.carrusel');
+    const prevBtn = container.querySelector('.prev-btn');
+    const nextBtn = container.querySelector('.next-btn');
+
+    prevBtn.addEventListener('click', () => {
+        carrusel.scrollBy({
+            left: -carrusel.offsetWidth / 3, // Mueve una imagen o un post
+            behavior: 'smooth'
+        });
     });
-});
 
-nextBtn.addEventListener('click', () => {
-    carrusel.scrollBy({
-        left: carrusel.offsetWidth / 2, // Mueve una imagen
-        behavior: 'smooth'
+    nextBtn.addEventListener('click', () => {
+        carrusel.scrollBy({
+            left: carrusel.offsetWidth / 3, // Mueve una imagen o un post
+            behavior: 'smooth'
+        });
     });
 });
